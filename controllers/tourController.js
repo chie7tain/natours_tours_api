1475d85a8fa70e76e553a4b7b1f686a8935723d0
Ifeanyi Okwuobi <fredrickokwuobi@gmail.com>
2025-07-10 18:46:46 +0100

    :sparkles: Update the review controller
    update the review controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,6 +10,12 @@ exports.checkNameandPrice = (req, res, next) => {
   next();
 };
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
 exports.getAllTours = async (req, res) => {
   try {
     const queryObj = { ...req.query };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,7 +9,10 @@ const {
   updateTour,
   deleteTour,
   checkNameandPrice,
+  aliasTopTours,
 } = require('../controllers/tourController');
+
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/').get(getAllTours).post(checkNameandPrice, createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
